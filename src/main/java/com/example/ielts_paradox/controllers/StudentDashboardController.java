f256a3c28867739f8e82df9be0bef68cea5c45e0
Emon Morol <ekramulhasane69@gmail.com>
2023-10-22 16:41:47 +0600

    student dashboard created

---
+++ src/main/java/com/example/ielts_paradox/controllers/StudentDashboardController.java
@@ -0,0 +1,83 @@
+package com.example.ielts_paradox.controllers;
+
+import com.example.ielts_paradox.utils.LoadDashboardPane;
+import com.example.ielts_paradox.utils.SceneChanger;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class StudentDashboardController implements Initializable {
+    @FXML
+    BorderPane mainPane = new BorderPane();
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        LoadDashboardPane ob = new LoadDashboardPane();
+        AnchorPane panel = ob.getSidePane("/fxmls/students/pages/my_classes.fxml");
+        mainPane.setCenter(panel);
+    }
+    @FXML
+    public void onClickOne(ActionEvent e){
+        LoadDashboardPane ob = new LoadDashboardPane();
+        AnchorPane panel = ob.getSidePane("/fxmls/students/pages/my_classes.fxml");
+        mainPane.setCenter(panel);
+    }
+    @FXML
+    public void onClick2(ActionEvent event) {
+        LoadDashboardPane ob = new LoadDashboardPane();
+        AnchorPane panel = ob.getSidePane("/fxmls/students/pages/page2.fxml");
+        mainPane.setCenter(panel);
+    }
+
+    @FXML
+    public void onClick3(ActionEvent event) {
+        LoadDashboardPane ob = new LoadDashboardPane();
+        AnchorPane panel = ob.getSidePane("/fxmls/students/pages/page3.fxml");
+        mainPane.setCenter(panel);
+    }
+
+    @FXML
+    public void onClick4(ActionEvent event) {
+        LoadDashboardPane ob = new LoadDashboardPane();
+        AnchorPane panel = ob.getSidePane("/fxmls/students/pages/page4.fxml");
+        mainPane.setCenter(panel);
+    }
+
+    @FXML
+    public void onClick5(ActionEvent event) {
+        LoadDashboardPane ob = new LoadDashboardPane();
+        AnchorPane panel = ob.getSidePane("/fxmls/students/pages/page5.fxml");
+        mainPane.setCenter(panel);
+    }
+
+    @FXML
+    public void onClick6(ActionEvent event) {
+        LoadDashboardPane ob = new LoadDashboardPane();
+        AnchorPane panel = ob.getSidePane("/fxmls/students/pages/page6.fxml");
+        mainPane.setCenter(panel);
+    }
+
+    @FXML
+    public void onClick7(ActionEvent event) {
+        LoadDashboardPane ob = new LoadDashboardPane();
+        AnchorPane panel = ob.getSidePane("/fxmls/students/pages/page7.fxml");
+        mainPane.setCenter(panel);
+    }
+
+    @FXML
+    public void onClick8(ActionEvent event) {
+        LoadDashboardPane ob = new LoadDashboardPane();
+        AnchorPane panel = ob.getSidePane("/fxmls/students/pages/page8.fxml");
+        mainPane.setCenter(panel);
+    }
+    public void logout(ActionEvent e) throws IOException {
+        new SceneChanger().switchScene(e,"/fxmls/login/login_page.fxml");
+    }
+
+
+}
