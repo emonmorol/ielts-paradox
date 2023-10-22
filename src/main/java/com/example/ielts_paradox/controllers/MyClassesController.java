71a16e8ec92c965f4af74c8638b4b77c21d388d3
Emon Morol <ekramulhasane69@gmail.com>
2023-10-22 16:43:26 +0600

    my class controller created

---
+++ src/main/java/com/example/ielts_paradox/controllers/MyClassesController.java
@@ -0,0 +1,73 @@
+package com.example.ielts_paradox.controllers;
+
+import com.example.ielts_paradox.utils.LoadDashboardPane;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+
+public class MyClassesController implements Initializable{
+
+    @FXML
+    private VBox firstBox,secondBox;
+    ArrayList<String> it = new ArrayList<>();
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        it.add("aksjhdsf");
+        it.add("aksjhdsf");
+        it.add("aksjhdsf");
+        it.add("aksjhdsf");
+        it.add("aksjhdsf");
+        it.add("aksjhdsf");
+        it.add("aksjhdsf");
+        it.add("aksjhdsf");
+        it.add("aksjhdsf");
+        it.add("aksjhdsf");
+        it.add("aksjhdsf");
+        it.add("aksjhdsf");
+        it.add("aksjhdsf");
+
+        it.add("aksjhdsf");
+        it.add("aksjhdsf");
+
+        it.add("aksjhdsf");
+        it.add("aksjhdsf");
+        it.add("aksjhdsf");
+        it.add("aksjhdsf");
+        it.add("aksjhdsf");
+
+        boolean isFirst = true;
+        for(String i:it){
+
+            try {
+                FXMLLoader fxmlLoader = new FXMLLoader();
+                fxmlLoader.setLocation(getClass().getResource("/fxmls/students/cards/courseCard.fxml"));
+                HBox paneee = fxmlLoader.load();
+                if(isFirst){
+                    firstBox.getChildren().add(paneee);
+                    isFirst = false;
+                }else{
+                    secondBox.getChildren().add(paneee);
+                    isFirst= true;
+                }
+//                mainBox.getChildren().add(paneee);
+
+//                System.out.println("Added\n");
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        }
+    }
+
+}
