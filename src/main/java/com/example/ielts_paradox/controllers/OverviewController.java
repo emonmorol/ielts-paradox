36f56ee14320309520e1918562eaa1293d0d3f5c
Emon Morol <ekramulhasane69@gmail.com>
2023-10-24 15:33:58 +0600

    overview controller and scene chanaged

---
+++ src/main/java/com/example/ielts_paradox/controllers/OverviewController.java
@@ -0,0 +1,19 @@
+package com.example.ielts_paradox.controllers;
+
+import com.example.ielts_paradox.utils.LoadDashboardPane;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+
+public class OverviewController {
+    @FXML
+    AnchorPane mainAnchor = new AnchorPane();
+    @FXML
+    public void onClickSeeCourse(ActionEvent event) {
+        LoadDashboardPane ob = new LoadDashboardPane();
+        AnchorPane panel = ob.getSidePane("/fxmls/students/pages/courses.fxml");
+        mainAnchor.getChildren().add(panel);
+//        mainPane.setCenter(panel);
+    }
+}
